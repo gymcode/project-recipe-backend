1ef4786c13dd16e06595d5f4721d4c0bdeff6cf4
gymcode <kyleabs20@gmail.com>
2022-12-04 23:09:17 +0000

    added swagger ui documentation

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,21 @@ import express, {Express} from "express";
 import Database_Connection from "./services/databaseConfig";
 import { config } from "dotenv";
 import { BASE_URL } from "./shared/constants";
+import SwaggerUi from "swagger-ui-express"
+import SwaggerDocs from "./swagger.json"
 config()
+
+//routesconfig()
+import userRoutes from "./routes/userRoute";
 
 const port = process.env.PORT || "8080"
 const app: Express = express()
 
 app.use(express.json())
-
-//routesconfig()
-import userRoutes from "./routes/userRoute";
+app.use('/haute-cuisine-api-docs', SwaggerUi.serve, SwaggerUi.setup(SwaggerDocs));
 
 // all user routes
 app.use(`${BASE_URL}/user`, userRoutes)
-
 app.listen(port, () => {
     // establish the database connection 
     Database_Connection()
