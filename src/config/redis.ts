86c593936124703637113d8e9d0216d8f2d67fe1
gymcode <kyleabs20@gmail.com>
2022-12-04 22:27:27 +0000

    restructured the redis function to handle the success and errors response separately

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,21 +1,30 @@
 import * as redis from "redis";
 
+function instanceEventListeners(conn: any) {
+    conn.on('connect', () => {
+        console.log('CacheStore - Connection status: connected');
+    });
+
+    conn.on('end', () => {
+        console.log('CacheStore - Connection status: disconnected');
+    });
+
+    conn.on('reconnecting', () => {
+        console.log('CacheStore - Connection status: reconnecting');
+    });
+
+    conn.on('error', (err: any) => {
+        console.log('CacheStore - Connection status: error ', { err });
+    });
+}
+
+
 const client = redis.createClient({
   url: process.env.REDIS_URL,
   password: process.env.REDIS_PASSWORD,
 });
-client.on("error", (err: any) => console.log("Redis Client Error", err));
-client.on("connect", () => {
-  console.log("CacheStore - Connection status: connected");
-});
+instanceEventListeners(client)
 
-client.on("end", () => {
-  console.log("CacheStore - Connection status: disconnected");
-});
-
-client.on("reconnecting", () => {
-  console.log("CacheStore - Connection status: reconnecting");
-});
 client.connect();
 
 export default client;
