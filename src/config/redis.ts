33b4c17d60c37d475a97c082dcb5f37b2c93cd3e
gymcode <kyleabs20@gmail.com>
2022-12-04 22:28:05 +0000

    formatting

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,29 +1,29 @@
 import * as redis from "redis";
 
 function instanceEventListeners(conn: any) {
-    conn.on('connect', () => {
-        console.log('CacheStore - Connection status: connected');
-    });
+  conn.on("connect", () => {
+    console.log("CacheStore - Connection status: connected");
+  });
 
-    conn.on('end', () => {
-        console.log('CacheStore - Connection status: disconnected');
-    });
+  conn.on("end", () => {
+    console.log("CacheStore - Connection status: disconnected");
+  });
 
-    conn.on('reconnecting', () => {
-        console.log('CacheStore - Connection status: reconnecting');
-    });
+  conn.on("reconnecting", () => {
+    console.log("CacheStore - Connection status: reconnecting");
+  });
 
-    conn.on('error', (err: any) => {
-        console.log('CacheStore - Connection status: error ', { err });
-    });
+  conn.on("error", (err: any) => {
+    console.log("CacheStore - Connection status: error ", { err });
+  });
 }
-
 
 const client = redis.createClient({
   url: process.env.REDIS_URL,
   password: process.env.REDIS_PASSWORD,
 });
-instanceEventListeners(client)
+
+instanceEventListeners(client);
 
 client.connect();
 
